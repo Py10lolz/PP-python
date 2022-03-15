6d229bbaf8b6bdf27f76920cfe765a347ae1eab3
Py10lolz <88994127+Py10lolz@users.noreply.github.com>
2022-03-15 10:11:37 +0800

    Create PP-program.py

---
+++ PP-program.py
@@ -0,0 +1,13 @@
+class PP:
+	def __init__(self, length, width):
+		self.length = length
+		self.width = width
+	def __repr__(self):
+		result = ""
+		for i in range(self.length):
+			result += " "*self.width + "#"*self.width + "\n"
+		for i in range(self.width):
+			result += "#" * self.width * 3 + "\n"
+		return result
+pp = PP(int(input("LENGTH:")), int(input("WIDTH:")))
+print(pp)
